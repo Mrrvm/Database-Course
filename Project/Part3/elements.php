7babc5d8fd6162ac142566c77125fa147322bdc7
Pedro Esperanca do Carmo <ist190989@sigma00.ist.utl.pt>
2017-11-27 15:34:55 +0000

    elements.php

--- Project/Part3/elements.php
+++ Project/Part3/elements.php
@@ -4,9 +4,9 @@ session_start();
 <html>
 <head>
 <meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 
   <title>Add New Region</title>
-	<link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
  <body>
    <h1>SIBD Project</h1>
