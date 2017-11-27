75cf29e355c30411953921e7545fc1b3db277516
PedroCarmo <pedro.esperanca.carmo@gmail.com>
2017-11-27 02:53:37 +0000

    add elements.php

---
+++ Project/Part3/region_result.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+?>
+<html>
+<head>
+<meta charset="utf-8">
+
+  <title>Add New Region</title>
+
+        <style>
+            .error {color: #FF0000;}
+	table, th, td {
+	    border: 1px solid black;
+	    padding: 5px;
+	    text-align:center;
+	}
+	table {
+	    border-spacing: 5px;
+	}
+        </style>
+</head>
+<body>
+   <h1>SIBD Project</h1>
+   
+<?php
+	$host = "db.ist.utl.pt";
+        $user = "ist190989";
+        $pass = "wkfi3575";
+        $dsn = "mysql:host=$host; dbname=$user";
+	try {
+            $connection = new PDO($dsn, $user, $pass);
+        } catch (PDOException $exception) {
+            echo("<p>Error: ");
+            echo($exception->getMessage());
+            echo("</p>");
+            exit();
+        }
+	$s_id = $_SESSION['series_id'];
+	$e_index = $_SESSION['e_ind'];
+	$validation = $_SESSION['validation']; 
+	$x1 = $_SESSION['x1'];
+	$x2 = $_SESSION['x2'];
+	$y1 = $_SESSION['y1'];
+	$y2 = $_SESSION['y2'];
+	$sql = "SELECT p.p_number, p.name as p_name, req.r_number, s.name as s_name,st.doctor_id FROM Patient as p, Request as req , Study as st , Series as s WHERE p.p_number = req.patient_id and req.r_number = st.request_number and st.request_number = s.request_number and st.description = s.description and s.series_id = $s_id";
+	$result = $connection->query($sql);
+
+	if($validation == "0"){
+		$msg = "ERROR REGION OVERLAP- Could not assign ";	
+	}else{
+		$sql2 = "INSERT INTO Region values($s_id, $e_index, $x1, $y1, $x2, $y2)";
+		$result_insert = $connection->query($sql2);
+		$msg = "SUCCESS - There IS ";
+		
+		
+	}	
+	foreach($result as $row){
+		$p_number = $row['p_number'];	
+		$p_name = $row['p_name'];
+		$r_number = $row['r_number'];
+		$s_name = $row['s_name'];
+		$doctor_id = $row['doctor_id'];
+	}
+
+	echo("<p>".$msg . "new clinical evidence on Patient $p_name (No. $p_number) in Element $e_index from Series $s_name (ID $s_id) requested by Doctor $doctor_id (Req. No. $r_number).</p>");
+	
+?>
+    </body>
+</html>
---
+++ Project/Part3/series.php
@@ -0,0 +1,59 @@
+<?php
+session_start();
+?>
+<html>
+<head>
+<meta charset="utf-8">
+
+  <title>Add New Region</title>
+
+        <style>
+            .error {color: #FF0000;}
+	table, th, td {
+	    border: 1px solid black;
+	    padding: 5px;
+	    text-align:center;
+	}
+	table {
+	    border-spacing: 5px;
+	}
+        </style>
+</head>
+<body>
+   <h1>SIBD Project</h1>
+   <h2>Series List</h2>
+<?php
+	$host = "db.ist.utl.pt";
+        $user = "ist190989";
+        $pass = "wkfi3575";
+        $dsn = "mysql:host=$host; dbname=$user";
+	try {
+            $connection = new PDO($dsn, $user, $pass);
+        } catch (PDOException $exception) {
+            echo("<p>Error: ");
+            echo($exception->getMessage());
+            echo("</p>");
+            exit();
+        }
+	$r_number = $_GET['r_number'];
+	$_SESSION['r_number'] = $r_number;
+	$sql = "SELECT *, count(e.series_id) FROM Series as s NATURAL JOIN Element as e WHERE s.request_number = $r_number";
+	$result = $connection->query($sql);
+	
+	echo("<table style=\"width:100%\">");
+	echo("<tr><th>Series ID</th><th>Name</th><th>Description</th><th>Elements</th></tr>");
+	foreach($result as $row){
+	    $series_id = $row['series_id'];
+	    $name = $row['name'];
+	    $description = $row['description'];
+	    $count = $row['count(e.series_id)'];
+	    
+	echo("<tr><td>$series_id</td><td>$name</td><td>$description</td><td><a href =\"elements.php?series_id=$series_id\" >$count</a></td></tr>");
+	}
+	echo("</table>"); 
+	
+
+?>
+<h3><a href = "sibd-part3-4.php">Return Studies List</a></h3>
+    </body>
+</html>
